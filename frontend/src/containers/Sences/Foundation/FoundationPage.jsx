74a9d17ba667f550d572446c541bc894115ae556
kevinsslin <kk123750964@gmail.com>
2023-05-31 16:12:48 +0800

    feat: adding BankInfo in Foundation page

--- frontend/src/containers/Sences/Foundation/FoundationPage.jsx
+++ frontend/src/containers/Sences/Foundation/FoundationPage.jsx
@@ -1,11 +1,13 @@
 import NFTMintButton from "components/NFTMintButton";
 import NFTGallery from "components/NFTGallery";
+import BankInfo from "components/BankInfo";
 
 const FoundationPage = () => {
   return (
     <div>贊助我們
       <NFTGallery />
       <NFTMintButton />
+      <BankInfo />
     </div>
   );
 };
