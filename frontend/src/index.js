6d02200acc6a0641ed1f8164d31fcb5ec11ff0bd
Nick <nick20350@gmail.com>
2023-06-03 00:37:22 +0800

    add login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ConfigProvider } from 'antd';
+import { Auth0Provider }  from '@auth0/auth0-react';
+const domain = "dev-mgi2drpxgmrso2he.us.auth0.com";
+const clientId = "tQ24C6Lm0UanIEokNUrE0oIdNUtE1Gbs";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,8 +16,14 @@ root.render(
           colorPrimary: 'rgb(98, 109, 75)',
         },
       }}
-    > 
-      <App />
+    >
+      <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        redirectUri={window.location.origin}
+      >
+        <App />
+      </Auth0Provider>
     </ConfigProvider>
   </React.StrictMode>
 );
