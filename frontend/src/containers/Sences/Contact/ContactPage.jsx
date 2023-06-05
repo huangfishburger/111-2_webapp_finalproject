aa54c0f70551a6ee5172216c94590a163847d0c5
HSIN, HUANG <89914115+xinhuang0716@users.noreply.github.com>
2023-06-05 17:24:09 +0800

    Update ContactPage.jsx
    adding contributor cards

--- frontend/src/containers/Sences/Contact/ContactPage.jsx
+++ frontend/src/containers/Sences/Contact/ContactPage.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import background from "./Contact - background.jpg";
+import shot4 from "./shot/shot4.jpg";
+import shot3 from "./shot/shot3.jpg";
+import shot2 from "./shot/shot2.jpg";
+import shot1 from "./shot/shot1.jpg";
+import shot0 from "./shot/shot0.jpg";
 
 const img_style = {
   width: "100%",
@@ -24,26 +29,39 @@ const contact_info = {
 
 const card = {
   background: "rgba(54,54,54,0.8)",
-  padding: "20px 20px",
-  margin: "0 1.5vw",
-  width: "20vw",
-  height: "30vh",
+  margin: "0 1vw",
+  width: "15vw",
+  height: "54vh",
   display: "flex",
   flexDirection: "column",
-  color: "WhiteSmoke",
-  cursor: "pointer",
   borderRadius: "5%",
 };
 
-const card_icon = {
-  color: "#ff6348",
-  fontSize: "50px",
-  borderRadius: "2%",
+const shot = {
+  float: "left",
+  width: "15vw",
+  height: "30vh",
+  borderRadius: "10%",
+  objectFit: "cover",
+  padding: "2%",
 };
 
-const p = {
+const name = {
+  maxHeight: "15vh",
   padding: "0",
   margin: "0",
+  lineHeight: "10vh",
+  fontSize: "150%",
+  fontWeight: "normal",
+  color: "whitesmoke",
+  fontFamily: "fantasy",
+};
+
+const icon = {
+  maxHeight: "10vh",
+  lineHeight: "8vh",
+  fontSize: "150%",
+  opacity: "70%",
 };
 
 function ContactPage() {
@@ -53,27 +71,160 @@ function ContactPage() {
         <img style={img_style} src={background} alt="" />
       </div>
       <div
+        className="PageInformation"
         style={{
           position: "absolute",
-          top: "45vh",
-          left: "15vw",
+          padding: "0",
+          margin: "0 1vw",
+          top: "92vh",
+          right: "0vw",
+          lineHeight: "3vh",
+          textAlign: "right",
+          color: "white",
+          fontSize: "75%",
+        }}
+      >
+        Copyright © 2023 by WebApp Group11
+        <br />
+        <i>No.1, Sec. 4, Roosevelt Road, Taipei, Taiwan</i>
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "28vh",
+          left: "7.5vw",
         }}
       >
         <div className="contact-info" style={contact_info}>
           <div className="card" style={card}>
-            <i className="card-icon far fa-envelope" style={card_icon}></i>
-            <p style={p}>r10724039@ntu.edu.tw</p>
+            <img src={shot4} alt="" style={shot} />
+            <div style={name}>
+              <p>經濟二 黃榆婷</p>
+            </div>
+            <div style={icon}>
+              <a
+                id="4_1"
+                href="mailto:b10303029@ntu.edu.tw"
+                style={{ margin: "0 1vw" }}
+              >
+                <i
+                  className={"fa fa-envelope"}
+                  style={{ color: "#ff6348" }}
+                ></i>
+              </a>
+              <a
+                id="4_2"
+                href="https://github.com/huangfishburger"
+                style={{ margin: "0 1vw" }}
+                target="_blank"
+              >
+                <i className="fa fa-link" style={{ color: "#ff6348" }}></i>
+              </a>
+            </div>
           </div>
           <div className="card" style={card}>
-            <i className="card-icon fas fa-phone" style={card_icon}></i>
-            <p style={p}>02-0000-0000 #123</p>
+            <img src={shot3} alt="" style={shot} />
+            <div style={name}>
+              <p>生機四 張熯尹</p>
+            </div>
+            <div style={icon}>
+              <a
+                id="4_1"
+                href="mailto:b08611038@ntu.edu.tw"
+                style={{ margin: "0 1vw" }}
+              >
+                <i
+                  className={"fa fa-envelope"}
+                  style={{ color: "#ff6348" }}
+                ></i>
+              </a>
+              <a
+                id="4_2"
+                href="https://github.com/Smallfu666"
+                style={{ margin: "0 1vw" }}
+                target="_blank"
+              >
+                <i className="fa fa-link" style={{ color: "#ff6348" }}></i>
+              </a>
+            </div>
           </div>
           <div className="card" style={card}>
-            <i
-              className="card-icon fas fa-map-marker-alt"
-              style={card_icon}
-            ></i>
-            <p style={p}>No. 1, Sec. 4, Roosevelt Rd., Taipei</p>
+            <img src={shot0} alt="" style={shot} />
+            <div style={name}>
+              <p>地理四 顏廷龍</p>
+            </div>
+            <div style={icon}>
+              <a
+                id="4_1"
+                href="mailto:b08208047@ntu.edu.tw"
+                style={{ margin: "0 1vw" }}
+              >
+                <i
+                  className={"fa fa-envelope"}
+                  style={{ color: "#ff6348" }}
+                ></i>
+              </a>
+              <a
+                id="4_2"
+                href="https://github.com/nolonger891204"
+                style={{ margin: "0 1vw" }}
+                target="_blank"
+              >
+                <i className="fa fa-link" style={{ color: "#ff6348" }}></i>
+              </a>
+            </div>
+          </div>
+          <div className="card" style={card}>
+            <img src={shot1} alt="" style={shot} />
+            <div style={name}>
+              <p>經濟二 林柏呈</p>
+            </div>
+            <div style={icon}>
+              <a
+                id="4_1"
+                href="mailto:b10303046@ntu.edu.tw"
+                style={{ margin: "0 1vw" }}
+              >
+                <i
+                  className={"fa fa-envelope"}
+                  style={{ color: "#ff6348" }}
+                ></i>
+              </a>
+              <a
+                id="4_2"
+                href="https://github.com/kevinsslin"
+                style={{ margin: "0 1vw" }}
+                target="_blank"
+              >
+                <i className="fa fa-link" style={{ color: "#ff6348" }}></i>
+              </a>
+            </div>
+          </div>
+          <div className="card" style={card}>
+            <img src={shot2} alt="" style={shot} />
+            <div style={name}>
+              <p>國企碩二 黃昕</p>
+            </div>
+            <div style={icon}>
+              <a
+                id="4_1"
+                href="mailto:r10724039@ntu.edu.tw"
+                style={{ margin: "0 1vw" }}
+              >
+                <i
+                  className={"fa fa-envelope"}
+                  style={{ color: "#ff6348" }}
+                ></i>
+              </a>
+              <a
+                id="4_2"
+                href="https://github.com/xinhuang0716"
+                style={{ margin: "0 1vw" }}
+                target="_blank"
+              >
+                <i className="fa fa-link" style={{ color: "#ff6348" }}></i>
+              </a>
+            </div>
           </div>
         </div>
       </div>
